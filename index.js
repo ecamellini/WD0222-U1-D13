7ba82b53bf742362937e83f63751cf8db3fb3544
Eric Camellini <eric.camellini@gmail.com>
2022-05-11 12:11:02 +0200

    Making it clearer

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
 function addNumberToResult(event) {
-  let numberPressed = event.target.innerText
+  let clickedNode = event.target
+
+  let numberPressed = clickedNode.innerText
   let result = document.getElementById('result')
 
   if (result.value === "0") {
+    // First number we press
     result.value = numberPressed
   } else {
     result.value += numberPressed // We appen the char numberpressed to the output
