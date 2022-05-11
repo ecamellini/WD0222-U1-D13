e9e57bb4fb32d22b1e77cf652ea37512d3478e2f
Eric Camellini <eric.camellini@gmail.com>
2022-05-11 13:10:14 +0200

    Handling operations

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// A MEMORY FOR OUR CALCULATOR
+// This is the memory where we store the previous results.
+let lastResult;
+let lastOperation;
+// These variables are declared outside every function.
+// This means that:
+// -- They will be visible, available, for the entire program
+// -- We can read them and modify them from any function
 
 function addNumberToResult(event) {
   let clickedNode = event.target
@@ -15,4 +23,48 @@ function addNumberToResult(event) {
 
 function clearResult() {
   document.getElementById('result').value = "0"
+}
+
+function saveOperationAndValue(event) {
+  let clickedButton = event.target
+
+  lastOperation = clickedButton.innerText
+  lastResult = document.getElementById('result').value
+
+  clearResult()
+}
+
+function showNextResult() {
+  let resultNode = document.getElementById('result')
+  let currentResult = resultNode.value
+  // We have:
+  // lastResult -- the first number
+  // lastOperation -- operation to perform
+  // currentResult -- the second number
+  // THEY ARE ALL STRINGS
+
+  let firstNumber = parseFloat(lastResult)
+  let secondNumber = parseFloat(currentResult)
+
+  switch(lastOperation) {
+    case "+":
+      // Do something
+      resultNode.value = firstNumber + secondNumber
+      break
+
+    case "-":
+      // Do something
+      resultNode.value = firstNumber - secondNumber
+      break
+
+    case "*":
+      resultNode.value = firstNumber * secondNumber
+      // Do something
+      break
+
+    case "/":
+      // Do
+      resultNode.value = firstNumber / secondNumber
+      break
+  }
 }
